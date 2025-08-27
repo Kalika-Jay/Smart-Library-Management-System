16d0b66728d645e458c32a5342a100d5aa143bfb
Kalika-Jay <kalika.jay2004@gmail.com>
2025-08-28 00:00:07 +0530

    library  addded

---
+++ src/Library.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+    private List<User> users;
+
+    public Library() {
+        this.books = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+}
